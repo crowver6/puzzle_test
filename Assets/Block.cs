236ca88efd4c1383e2f5abad1b2a6a1226eb4e84
Harutaka Yamashita <g_playerpos@hotmail.com>
2013-07-02 20:36:33 +0900

    ブロックをランダムで生成

--- Assets/Block.cs
+++ Assets/Block.cs
@@ -1,15 +1,61 @@
 using UnityEngine;
 using System.Collections;
 
+
+
 public class Block : MonoBehaviour {
 
+	enum TYPE{
+		TYPE_1,
+		TYPE_2,
+		TYPE_3,
+		TYPE_4,
+		TYPE_5,
+		TYPE_6,
+		TYPE_SP1,
+		TYPE_SP2,
+		TYPE_SP3,
+		
+		TYPE_MAX,
+		
+		TYPE_NORMAL_NUM = TYPE_SP1,
+		TYPE_SP_NUM = TYPE_MAX - TYPE_NORMAL_NUM
+	}
+
+	public int type;
+	Material mat;
+	
+	
+	
+	
 	// Use this for initialization
 	void Start () {
-	
+		type = (int)(Random.Range((int)TYPE.TYPE_1,(int)TYPE.TYPE_NORMAL_NUM));
+		
+		mat = GetComponent<MeshRenderer>().material;// マテリアルを取得しておく
+		
+		SetTypeColor();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+	
+	
+	void SetTypeColor(){
+		Color set_color;
+		
+		switch(type){
+		case (int)TYPE.TYPE_1:	set_color = Color.red;		break;
+		case (int)TYPE.TYPE_2:	set_color = Color.blue;		break;
+		case (int)TYPE.TYPE_3:	set_color = Color.cyan;		break;
+		case (int)TYPE.TYPE_4:	set_color = Color.yellow;	break;
+		case (int)TYPE.TYPE_5:	set_color = Color.green;	break;
+		case (int)TYPE.TYPE_6:	set_color = Color.white;	break;
+		default:				set_color = Color.black;	break;
+		}
+		mat.SetColor("_Color",set_color);
+	}
+	
 }
--- Assets/BlockManager.cs
+++ Assets/BlockManager.cs
@@ -4,22 +4,36 @@ using System.Collections;
 public class BlockManager : MonoBehaviour {
 	
 	public GameObject src;
+	
+	const int table_width = 8;
+	const int table_height = 6;
+	
+	GameObject[,] block_tbl = new GameObject[table_width,table_height];
 
 	// Use this for initialization
 	void Start () {
-		int i,j;
-		int w = 8;
-		int h = 6;
-		for( i = 0; i < w; i++ ){
-			for( j = 0; j < h; j++ ){
-				Instantiate(src,new Vector3((float)(i-(w/2)),0,(float)(j-(h/2))),Quaternion.identity);
-			}
-		}
-	
+		Layout();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+	
+	
+	void Layout(){
+		int i,j;
+		int w = table_width;
+		int h = table_height;
+		for( i = 0; i < w; i++ ){
+			for( j = 0; j < h; j++ ){
+				Instantiate(src,new Vector3((float)(i-(w/2)),0,(float)(j-(h/2))),Quaternion.identity);
+				block_tbl[i,j] = (GameObject)Instantiate(src,new Vector3((float)(i-(w/2)),0,(float)(j-(h/2))),Quaternion.identity);
+			}
+		}
+	}
+	
+	void SerchErase(){
+		
 	}
 }
